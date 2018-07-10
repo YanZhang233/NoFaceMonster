289b094fedbd874118361d4d7a6becaa7ff30062
Yan <zyellie7@gmail.com>
2018-07-10 13:39:44 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var monster = document.getElementById("monster");
+var msg = document.getElementById("msg");
+var loading = document.getElementById("loading");
+var heart = document.getElementById("heart");
+
+monster.addEventListener("click", function () {
+    msg.classList.remove("hide");
+
+    setTimeout(popHeart, 2500);
+});
+
+function popHeart() {
+    loading.classList.add("hide");
+    heart.classList.remove("hide");
+}
